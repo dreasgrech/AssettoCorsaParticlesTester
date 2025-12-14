e8535ab83820a64198d9d1c3524d31ae20160dff
Andreas Grech <agnt666@gmail.com>
2025-12-14 19:08:16 +0100

    adding a UIOperations

--- AssettoCorsaParticlesTester.lua
+++ AssettoCorsaParticlesTester.lua
@@ -1,3 +1,4 @@
+UIOperations = require('UIOperations')
 
 -- local bindings
 local ac = ac
@@ -19,122 +20,12 @@ local ui_pushID = ui.pushID
 local ui_popID = ui.popID
 local ui_text = ui.text
 local ui_checkbox = ui.checkbox
-local ui_colorButton = ui.colorButton
 local string_format = string.format
 
 
 local UI_HEADER_TEXT_FONT_SIZE = 15
 local DEFAULT_SLIDER_WIDTH = 200
 local DEFAULT_SLIDER_FORMAT = '%.2f'
-
-
-local function getWorldPositionFromMouseClick()
-    -- Avoid conflicts if you’re using CSP’s gizmo/positioning helper
-    if render.isPositioningHelperBusy() then return nil end
-
-    -- Only act on a left-click (and avoid UI clicks)
-    if ui.mouseBusy() then return nil end
-    if not ui.mouseClicked(ui.MouseButton.Left) then return nil end
-
-    local ray = render.createMouseRay()
-
-    -- Option A: intersect visual track mesh
-    local hitDistance = ray:track(1)
-    if hitDistance >= 0 then
-        return ray.pos + ray.dir * hitDistance
-    end
-
-    -- Option B: intersect physics meshes (also gives normal if you pass out params)
-    local outPos = vec3()
-    local outNormal = vec3()
-    local physDistance = ray:physics(outPos, outNormal)
-    if physDistance >= 0 then
-        -- outPos already contains the contact point
-        return outPos, outNormal
-    end
-
-    return nil
-end
-
----Color button control. Returns true if color has changed (as usual with Lua, colors are passed)
----by reference so update value would be put in place of old one automatically.
----@param label string
----@param color rgb|rgbm
----@param flags ui.ColorPickerFlags?
----@param size vec2?
----@return boolean
-local renderColorPicker = function(label, tooltip, color, flags, size)
-    ui_colorButton(label, color, flags, size)
-
-    if ui_itemHovered() then
-        -- render the tooltip
-        ui_setTooltip(tooltip)
-    end
-    
-    ui_sameLine()
-    ui.text(label)
-    
-    return color
-end
-
----Renders a slider with a tooltip
----@param label string @Slider label.
----@param tooltip string
----@param value refnumber|number @Current slider value.
----@param minValue number? @Default value: 0.
----@param maxValue number? @Default value: 1.
----@param sliderWidth number
----@param labelFormat string|'%.3f'|nil @C-style format string. Default value: `'%.3f'`.
----@param defaultValue number @The default value to reset to on right-click and is shown in the tooltip.
----@return number @Possibly updated slider value.
-local renderSlider = function(label, tooltip, value, minValue, maxValue, sliderWidth, labelFormat, defaultValue)
-    -- set the width of the slider
-    ui_pushItemWidth(sliderWidth)
-
-    -- render the slider
-    -- Andreas: doing the ' ' .. label thing here because when writing a label after the slider manually, there's an extra space so here I'm adding an extra space so they can match
-    local newValue = ui_slider(' ' .. label, value, minValue, maxValue, labelFormat)
-
-    -- reset the item width
-    ui_popItemWidth()
-
-    if ui_itemHovered() then
-        tooltip = string_format('%s\n\nDefault: %.2f', tooltip, defaultValue)
-        
-        -- render the tooltip
-        ui_setTooltip(tooltip)
-
-        -- reset the slider to default value on right-click
-        if ui_mouseClicked(ui_MouseButton.Right) then
-            -- Logger.log(string.format('Resetting slider "%s" to default value: %.2f', label, defaultValue))
-            newValue = defaultValue
-        end
-    end
-
-    return newValue
-end
-
----Renders a vec3 slider (3 sliders in one line).
----@param label string @Slider label.
----@param value vec3 @Current vec3 value.
----@param minValue number @Minimum slider value.
----@param maxValue number @Maximum slider value.
----@param format string|'X: %.3f'|'Y: %.3f'|'Z: %.3f'|nil @C-style format string. Default value: `'X: %.3f'`, `'Y: %.3f'`, `'Z: %.3f'
----@return vec3 newValue
----@return boolean changed
-local function uiVec3(label, value, minValue, maxValue, format)
-    ui_pushID(label)
-
-    local x = renderSlider('##x', '', value.x, minValue, maxValue, 350, format or 'X: %.3f', 0)
-    --ui_sameLine()
-    local y = renderSlider('##y', '', value.y, minValue, maxValue, 350, format or 'Y: %.3f', 0)
-    --ui_sameLine()
-    local z = renderSlider('##z', '', value.z, minValue, maxValue, 350, format or 'Z: %.3f', 0)
-
-    ui_popID()
-
-    return vec3(x, y, z)
-end
 
 --[======[
 ---Flame emitter holding specialized settings. Set settings in a table when creating an emitter and/or change them later.
@@ -218,15 +109,15 @@ function script.MANIFEST__FUNCTION_MAIN(dt)
     ui_newLine(1)
     
     ui_text('Velocity')
-    storage.flame_velocity = uiVec3('Velocity', storage.flame_velocity, -100, 100)
+    storage.flame_velocity = UIOperations.uiVec3('Velocity', storage.flame_velocity, -100, 100)
     
     ui_newLine(1)
 
-    storage.flame_color = renderColorPicker('Color', 'Flame color multiplier\n\nFor red/yellow/blue adjustment use `Temperature Multiplier` instead.', storage.flame_color, colorPickerFlags, colorPickerSize)
-    storage.flame_size = renderSlider('Size', 'Particles size', storage.flame_size, 0, 50, DEFAULT_SLIDER_WIDTH, DEFAULT_SLIDER_FORMAT, 50)
-    storage.flame_temperatureMultiplier = renderSlider('Temperature Multiplier', 'Temperature multipler to vary base color from red to blue.', storage.flame_temperatureMultiplier, 0, 10, DEFAULT_SLIDER_WIDTH, DEFAULT_SLIDER_FORMAT, 1)
-    storage.flame_flameIntensity = renderSlider('Flame Intensity', 'Flame intensity affecting flame look and behaviour.', storage.flame_flameIntensity, 0, 10, DEFAULT_SLIDER_WIDTH, DEFAULT_SLIDER_FORMAT, 1)
-    storage.flame_amount = renderSlider('Amount', 'The description', storage.flame_amount, 1, 10, DEFAULT_SLIDER_WIDTH, '%.0f', 1)
+    storage.flame_color = UIOperations.renderColorPicker('Color', 'Flame color multiplier\n\nFor red/yellow/blue adjustment use `Temperature Multiplier` instead.', storage.flame_color, colorPickerFlags, colorPickerSize)
+    storage.flame_size = UIOperations.renderSlider('Size', 'Particles size', storage.flame_size, 0, 50, DEFAULT_SLIDER_WIDTH, DEFAULT_SLIDER_FORMAT, 50)
+    storage.flame_temperatureMultiplier = UIOperations.renderSlider('Temperature Multiplier', 'Temperature multipler to vary base color from red to blue.', storage.flame_temperatureMultiplier, 0, 10, DEFAULT_SLIDER_WIDTH, DEFAULT_SLIDER_FORMAT, 1)
+    storage.flame_flameIntensity = UIOperations.renderSlider('Flame Intensity', 'Flame intensity affecting flame look and behaviour.', storage.flame_flameIntensity, 0, 10, DEFAULT_SLIDER_WIDTH, DEFAULT_SLIDER_FORMAT, 1)
+    storage.flame_amount = UIOperations.renderSlider('Amount', 'The description', storage.flame_amount, 1, 10, DEFAULT_SLIDER_WIDTH, '%.0f', 1)
     
     -- finish the columns
     ui_columns(1, false)
@@ -236,7 +127,7 @@ end
 -- wiki: called after a whole simulation update
 ---
 function script.MANIFEST__UPDATE(dt)
-    local positionFromClick = getWorldPositionFromMouseClick()
+    local positionFromClick = UIOperations.getWorldPositionFromMouseClick()
     if positionFromClick ~= nil then
         storage.flame_position = positionFromClick
     end
---
+++ UIOperations.lua
@@ -0,0 +1,127 @@
+﻿local ui = ui
+local ui_colorButton = ui.colorButton
+local ui_itemHovered = ui.itemHovered
+local ui_setTooltip = ui.setTooltip
+local ui_sameLine = ui.sameLine
+local ui_text = ui.text
+local ui_pushItemWidth = ui.pushItemWidth
+local ui_popItemWidth = ui.popItemWidth
+local ui_slider = ui.slider
+local ui_mouseClicked = ui.mouseClicked
+local ui_MouseButton = ui.MouseButton
+local ui_pushID = ui.pushID
+local ui_popID = ui.popID
+local string_format = string.format
+local render = render
+
+local UIOperations = {}
+
+---Color button control. Returns true if color has changed (as usual with Lua, colors are passed)
+---by reference so update value would be put in place of old one automatically.
+---@param label string
+---@param color rgb|rgbm
+---@param flags ui.ColorPickerFlags?
+---@param size vec2?
+---@return boolean
+UIOperations.renderColorPicker = function(label, tooltip, color, flags, size)
+    ui_colorButton(label, color, flags, size)
+
+    if ui_itemHovered() then
+        -- render the tooltip
+        ui_setTooltip(tooltip)
+    end
+
+    ui_sameLine()
+    ui_text(label)
+
+    return color
+end
+
+UIOperations.getWorldPositionFromMouseClick = function()
+    -- Avoid conflicts if you’re using CSP’s gizmo/positioning helper
+    if render.isPositioningHelperBusy() then return nil end
+
+    -- Only act on a left-click (and avoid UI clicks)
+    if ui.mouseBusy() then return nil end
+    if not ui.mouseClicked(ui.MouseButton.Left) then return nil end
+
+    local ray = render.createMouseRay()
+
+    -- Option A: intersect visual track mesh
+    local hitDistance = ray:track(1)
+    if hitDistance >= 0 then
+        return ray.pos + ray.dir * hitDistance
+    end
+
+    -- Option B: intersect physics meshes (also gives normal if you pass out params)
+    local outPos = vec3()
+    local outNormal = vec3()
+    local physDistance = ray:physics(outPos, outNormal)
+    if physDistance >= 0 then
+        -- outPos already contains the contact point
+        return outPos, outNormal
+    end
+
+    return nil
+end
+
+---Renders a slider with a tooltip
+---@param label string @Slider label.
+---@param tooltip string
+---@param value refnumber|number @Current slider value.
+---@param minValue number? @Default value: 0.
+---@param maxValue number? @Default value: 1.
+---@param sliderWidth number
+---@param labelFormat string|'%.3f'|nil @C-style format string. Default value: `'%.3f'`.
+---@param defaultValue number @The default value to reset to on right-click and is shown in the tooltip.
+---@return number @Possibly updated slider value.
+UIOperations.renderSlider = function(label, tooltip, value, minValue, maxValue, sliderWidth, labelFormat, defaultValue)
+    -- set the width of the slider
+    ui_pushItemWidth(sliderWidth)
+
+    -- render the slider
+    -- Andreas: doing the ' ' .. label thing here because when writing a label after the slider manually, there's an extra space so here I'm adding an extra space so they can match
+    local newValue = ui_slider(' ' .. label, value, minValue, maxValue, labelFormat)
+
+    -- reset the item width
+    ui_popItemWidth()
+
+    if ui_itemHovered() then
+        tooltip = string_format('%s\n\nDefault: %.2f', tooltip, defaultValue)
+
+        -- render the tooltip
+        ui_setTooltip(tooltip)
+
+        -- reset the slider to default value on right-click
+        if ui_mouseClicked(ui_MouseButton.Right) then
+            -- Logger.log(string.format('Resetting slider "%s" to default value: %.2f', label, defaultValue))
+            newValue = defaultValue
+        end
+    end
+
+    return newValue
+end
+
+---Renders vec3 sliders
+---@param label string @Slider label.
+---@param value vec3 @Current vec3 value.
+---@param minValue number @Minimum slider value.
+---@param maxValue number @Maximum slider value.
+---@param format string|'X: %.3f'|'Y: %.3f'|'Z: %.3f'|nil @C-style format string. Default value: `'X: %.3f'`, `'Y: %.3f'`, `'Z: %.3f'
+---@return vec3 newValue
+---@return boolean changed
+UIOperations.uiVec3 = function(label, value, minValue, maxValue, format)
+    ui_pushID(label)
+
+    local x = UIOperations.renderSlider('##x', '', value.x, minValue, maxValue, 350, format or 'X: %.3f', 0)
+    --ui_sameLine()
+    local y = UIOperations.renderSlider('##y', '', value.y, minValue, maxValue, 350, format or 'Y: %.3f', 0)
+    --ui_sameLine()
+    local z = UIOperations.renderSlider('##z', '', value.z, minValue, maxValue, 350, format or 'Z: %.3f', 0)
+
+    ui_popID()
+
+    return vec3(x, y, z)
+end
+
+return UIOperations
